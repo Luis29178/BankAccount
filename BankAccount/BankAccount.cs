ddb5747b7329c08d39e076501f394bb4c13ad8fa
Luis Bazaldua <LEBazaldua@student.fullsail.edu>
2022-04-06 20:10:04 -0400

    Add project files.

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿namespace BankAccount
+{
+    public class BankAccount
+    {
+        
+        private bool Frozen = false;
+        public bool frozen
+        {
+            get
+            {
+                return Frozen;
+            }
+            set
+            {
+                Frozen = value;
+            }
+
+        }
+        private float Balance = 0;
+        public float balance
+        {  
+            get 
+            { 
+               return Balance; 
+            }               
+            set 
+            {
+               Balance = value; 
+            }    
+        
+        }
+
+
+        public void Credit(int balanceIn)
+        {
+            Balance += balanceIn;
+        }
+        public void Debit(int balanceOut)
+        {
+            if (!Frozen)
+            {
+                Balance -= balanceOut;
+            }
+        }
+
+    }
+}
